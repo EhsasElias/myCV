d724690a13bfed3262723a35d1f54eed635e56f7
Ehsas <ehsas.1998000@gmail.com>
2022-01-13 18:01:38 +0300

    fourth edit

--- js/js.js
+++ js/js.js
@@ -153,5 +153,23 @@ document.addEventListener('dblclick', function (e) {
     element8.classList.toggle("dark-mode");
     element9.classList.toggle("dark-mode");
     
+});
 
+
+var touchtime = 0;
+document.body.on("click", function() {
+    if (touchtime == 0) {
+        // set first click
+        touchtime = new Date().getTime();
+    } else {
+        // compare first click to this click and see if they occurred within double click threshold
+        if (((new Date().getTime()) - touchtime) < 800) {
+            // double click occurred
+            alert("double clicked");
+            touchtime = 0;
+        } else {
+            // not a double click so set as a new first click
+            touchtime = new Date().getTime();
+        }
+    }
 });
